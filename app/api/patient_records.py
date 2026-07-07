53a474212ebe2c22a419f5380b608b33cfc0b30e
Johnny Mudawar <johnnymudawar@gmail.com>
2026-07-06 22:10:04 -0700

    Add delete patient endpoint

--- app/api/patient_records.py
+++ app/api/patient_records.py
@@ -16,3 +16,13 @@ def get_patients(db: Session = Depends(get_db)):
 @router.post("/patients", response_model=PatientResponse, status_code=status.HTTP_201_CREATED)
 def create_patient_record(patient: PatientCreate, db: Session = Depends(get_db)):
     return create_patient(db, patient)
+
+
+@router.delete("/patients/{patient_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_patient_record(patient_id: str, db: Session = Depends(get_db)):
+    patient = db.query(Patients).filter(Patients.id == patient_id).first()
+    if patient is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Patient not found")
+
+    db.delete(patient)
+    db.commit()
